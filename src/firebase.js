edd255d5106acfbd6745bb3e51694040e1f70015
sism1s <marko.lazic880@gmail.com>
2021-04-08 17:03:08 +0100

    Set up firebase

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCZr9k_uV1HoG-lt9-MnLu1oAQgpSUn-oU",
+  authDomain: "facebook-clone-293ef.firebaseapp.com",
+  projectId: "facebook-clone-293ef",
+  storageBucket: "facebook-clone-293ef.appspot.com",
+  messagingSenderId: "233177721033",
+  appId: "1:233177721033:web:315675d05427e515cf4518",
+};
