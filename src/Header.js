52fac1d08ecefe29fbae98f0c671b0156cc48705
sism1s <marko.lazic880@gmail.com>
2021-04-08 18:10:23 +0100

    Created Header

---
+++ src/Header.js
@@ -0,0 +1,67 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import HomeIcon from "@material-ui/icons/Home";
+import FlagIcon from "@material-ui/icons/Flag";
+import SubscriptionOutlinedIcon from "@material-ui/icons/SubscriptionsOutlined";
+import StorefrontOutlinedIcon from "@material-ui/icons/StorefrontOutlined";
+import SupervisedUserCircleIcon from "@material-ui/icons/SupervisedUserCircle";
+import { Avatar, IconButton } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+import ForumIcon from "@material-ui/icons/Forum";
+import NotificationsActiveIcon from "@material-ui/icons/NotificationsActive";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img
+          src="https://1000logos.net/wp-content/uploads/2016/11/Facebook-logo.png"
+          alt=""
+        />
+        <div className="header__input">
+          <SearchIcon />
+          <input type="text" placeholder="Search Facebook" />
+        </div>
+      </div>
+      <div className="header__middle">
+        <div className="header__option header__option--active">
+          <HomeIcon fontSize="Large" />
+        </div>
+        <div className="header__option">
+          <FlagIcon fontSize="Large" />
+        </div>
+        <div className="header__option">
+          <SubscriptionOutlinedIcon fontSize="Large" />
+        </div>
+        <div className="header__option">
+          <StorefrontOutlinedIcon fontSize="Large" />
+        </div>
+        <div className="header__option">
+          <SupervisedUserCircleIcon fontSize="Large" />
+        </div>
+      </div>
+      <div className="header__right">
+        <div className="header__info">
+          <Avatar />
+          <h4>Marko</h4>
+        </div>
+        <IconButton>
+          <AddIcon />
+        </IconButton>
+        <IconButton>
+          <ForumIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationsActiveIcon />
+        </IconButton>
+        <IconButton>
+          <ExpandMoreIcon />
+        </IconButton>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
