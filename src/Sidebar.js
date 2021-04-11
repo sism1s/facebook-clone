86b11c4d53893a7ea6d2ceebc5e0b57bf4a3e768
sism1s <marko.lazic880@gmail.com>
2021-04-11 02:49:50 +0100

    created widgets

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -25,7 +25,7 @@ function Sidebar() {
       <SidebarRow Icon={ChatIcon} title="Messenger" />
       <SidebarRow Icon={StorefrontIcon} title="Marketplace" />
       <SidebarRow Icon={VideoLibraryIcon} title="Videos" />
-      <SidebarRow Icon={ExpandMoreOutlined} title="Marketplace" />
+      <SidebarRow Icon={ExpandMoreOutlined} title="More" />
     </div>
   );
 }
---
+++ src/Widgets.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Widgets.css";
+
+function Widgets() {
+  return (
+    <div className="widgets">
+      <div
+        class="fb-page"
+        data-href="https://www.facebook.com/RealMadrid/"
+        data-tabs="timeline"
+        data-width="360px"
+        data-height="1500px"
+        data-small-header="false"
+        data-adapt-container-width="true"
+        data-hide-cover="false"
+        data-show-facepile="true"
+      >
+        <blockquote
+          cite="https://www.facebook.com/RealMadrid/"
+          class="fb-xfbml-parse-ignore"
+        >
+          <a href="https://www.facebook.com/RealMadrid/">Real Madrid C.F.</a>
+        </blockquote>
+      </div>
+    </div>
+  );
+}
+
+export default Widgets;
